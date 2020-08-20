cbfef1a62f6ef3e5a8eb5c76f3c8d1f49f1026b9
Prince Roshan <princeroshan74@gmail.com>
2020-08-20 15:01:57 +0530

    Add files via upload

---
+++ CommandLineArguments/CommandLineArguments/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineArguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine("Argument Passed by User is: ");
+                foreach (object obj in args)
+                {
+                    Console.WriteLine(obj);
+                }
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("NO Argument Passed by User");
+                Console.ReadKey();
+            }
+        }
+    }
+}
