42f128a368e27838c6464348d1e5bbc06c866908
Prince Roshan <princeroshan7@outlook.com>
2020-08-24 22:26:15 +0530

    C# program to show the working of abstract class with the get and set accessors

---
+++ Abstraction_Demo2/Abstraction_Demo2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction_Demo2
+{
+    // C# program to show the working of abstract class with the get and set accessors 
+    abstract class absClass
+    {
+        protected int myNumber;
+        public abstract int numbers
+        {
+            get;
+            set;
+        }
+    }
+    class absDerived : absClass
+    {
+        // Implementing abstract properties 
+        public override int numbers
+        {
+            get
+            {
+                return myNumber;
+            }
+            set
+            {
+                myNumber = value;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            absDerived d = new absDerived();
+            d.numbers = 5;
+            Console.WriteLine(d.numbers);
+        }
+    }
+}
