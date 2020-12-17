0e2f4baf2241591f2e0d5791563263b4b95bfb62
Prince Roshan <princeroshan7@outlook.com>
2020-12-17 21:45:10 +0530

    Minor Changes like Static string message.

--- StaticVariable/StaticVariable/Program.cs
+++ StaticVariable/StaticVariable/Program.cs
@@ -9,26 +9,27 @@ namespace StaticVariable
     class Program
     {
         // static variable declared with static keyword
-        static double salary;
+        static double salary=40000;
         static string name = "Prince";
         static int age=25;
-        //Method
+        static string message;
+        //Non-Static Method
         public int add()
         {
             age=age+5; // Now, age=30
             return age;
         }
-        //Main Method/Driver Method
+        //Main Method
         static void Main(string[] args)
         {
             Program obj=new Program();
             int resage=obj.add();
-           
-            Program.salary = 30000;
-            Program.age = 28; //here age=30 gets change to 28
-            Console.WriteLine("Name :" + Program.name + " " + "Age :" + Program.age + " " + "Salary :" + Program.salary);
+
+            //static variable initialization
+            Program.message = "Static message";
+            Console.WriteLine("Name :" + Program.name + " " + "Age :" + Program.age + " " + "Salary :" + Program.salary+" "+"Message:"+message);
             //OR//
-            Console.WriteLine("Name :" + name + " " + "Age :" + age + " " + "Salary :" + salary);//age=28
+            Console.WriteLine("Name :" + name + " " + "Age :" + age + " " + "Salary :" + salary);
 
             Console.WriteLine(resage); //30
             Console.ReadKey();
