95d45c53bc0b04d96f4056679ed612b56543be6e
Prince Roshan <princeroshan7@outlook.com>
2020-12-19 21:08:43 +0530

    Updation in struct data member

--- Structure/Structure/Program.cs
+++ Structure/Structure/Program.cs
@@ -11,18 +11,21 @@ namespace Structure
         public string Name;
         public int age;
         public int weight;
+        public Details det;
     }
     //nested structure
-    public struct Details
-    {
-        public string Country;
-    }
-    struct Address
+    public struct Address
     {
         public string city;
         public string state;
-        public Details d1;
     }
+    public struct Details
+    {
+        public string mob_no;
+        public string sex;
+        public Address addr;
+    }
+    
     class Program
     {
         static void Main(string[] args)
@@ -31,18 +34,18 @@ namespace Structure
             p1.Name = "Prince";
             p1.age = 28;
             p1.weight = 70;
+            //nested struct declaration
+            p1.det.mob_no = "78789787978";
+            p1.det.sex = "Male";
+            p1.det.addr.city = "Ranchi";
+            p1.det.addr.state = "Jharkhand";
             Console.WriteLine("Name :" + p1.Name);
             Console.WriteLine("Age :" + p1.age);
+            Console.WriteLine("Sex :" + p1.det.sex);
             Console.WriteLine("Weight :" + p1.weight);
-
-            //nested struct declaration
-            Address a1;
-            a1.city = "Ranchi";
-            a1.state = "Jharkhand";
-            a1.d1.Country = "India";
-            Console.WriteLine("City :" + a1.city);
-            Console.WriteLine("State :" + a1.state);
-            Console.WriteLine("Country :" + a1.d1.Country);
+            Console.WriteLine("Mobile No :" + p1.det.mob_no);
+            Console.WriteLine("City :" + p1.det.addr.city);
+            Console.WriteLine("State :" + p1.det.addr.state);
             Console.ReadKey();
         }
     }
