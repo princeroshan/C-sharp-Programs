1f46e9923b94c120fbed8dba00c87b98e18c3016
Prince Roshan <princeroshan7@outlook.com>
2021-01-13 21:24:17 +0530

    Private Member access without get set properties.

---
+++ PrivateMemberAccess/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrivateMemberAccess
+{
+    class Abc
+    {
+        private int id = 100;
+        private int marks = 500;
+        public int ShowId()
+        {
+            return id;   
+        }
+        public int ShowMarks()
+        {
+            return marks;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Abc obj = new Abc();
+            int id2 = obj.ShowId();
+            int marks2 = obj.ShowMarks();// only you can access but can't update for that you have to use get set properties.
+            
+            Console.WriteLine("Private Member Shown :" + id2 + " " + marks2);
+        }
+    }
+}
