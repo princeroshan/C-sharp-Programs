651506698d5d7349b84fbd3885a87c5ce7d9af25
Prince Roshan <princeroshan7@outlook.com>
2021-01-20 20:52:13 +0530

    NA

--- Static_Class/Static_Class/Program.cs
+++ Static_Class/Static_Class/Program.cs
@@ -11,7 +11,6 @@ namespace Static_Class
         private static int _resultStorage = 0;
 
         public static string Type = "Arithmetic";
-
         public static int Sum(int num1, int num2)
         {
             return num1 + num2;
