40633ba9f433cad3352543883dc7f0a8cab3e786
Prince Roshan <princeroshan7@outlook.com>
2021-01-20 21:07:39 +0530

    Break Sta

---
+++ BreakStatement/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 4)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
