a771c8dfe992a3e5905466bffa3ab0cfcc013343
Prince Roshan <princeroshan7@outlook.com>
2021-01-21 23:13:44 +0530

    Method Overriding

---
+++ MethodOverriding/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverriding
+{
+    class baseClass
+
+    {
+        public void show()
+        {
+            Console.WriteLine("Base class");
+        }
+    }
+
+    // derived class name 'derived'
+    // 'baseClass' inherit here
+    class derived : baseClass
+    {
+
+        // overriding
+        new public void show()
+        {
+            Console.WriteLine("Derived class");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 'obj' is the object of
+            // class 'baseClass'
+            baseClass obj = new baseClass();
+
+
+            // invokes the method 'show()'
+            // of class 'baseClass'
+            obj.show();
+
+            obj = new derived();
+
+            // it also invokes the method 
+            // 'show()' of class 'baseClass'
+            obj.show();
+            derived obj1 = new derived();
+            obj1.show();
+        }
+    }
+}
