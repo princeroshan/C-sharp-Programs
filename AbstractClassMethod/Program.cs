134c34e518347758e7585c92a7336cb602d24899
Prince Roshan <princeroshan7@outlook.com>
2021-01-22 22:04:09 +0530

    NA

--- AbstractClassMethod/Program.cs
+++ AbstractClassMethod/Program.cs
@@ -9,6 +9,7 @@ namespace AbstractClassMethod
     public abstract class Abstract_class
     {
         public abstract void show();
+        public abstract int Area(int x, int y);
     }
     public class Geek1:Abstract_class
     {
@@ -16,12 +17,21 @@ namespace AbstractClassMethod
         {
             Console.WriteLine("Class Geek1 and abstarct method of Abstract Class called");
         }
+        public override int Area(int x, int y)
+        {
+            return x * y;
+        }
     }
     public class Geek2 : Abstract_class
     {
         public override void show()
         {
             Console.WriteLine("Class Geek2 and abstarct method of Abstract Class called");
+        }
+        public override int Area(int x, int y)
+        {
+            throw new NotImplementedException(); // you have to implement all abstract method of abstract class
+                                                // throw this exception to overcome compilation error.
         }
     }
     class Program
@@ -31,6 +41,7 @@ namespace AbstractClassMethod
             Abstract_class obj;
             obj = new Geek1();
             obj.show();
+            Console.WriteLine("Multiplication of Two Number are :" + obj.Area(10, 20));
             obj = new Geek2();
             obj.show();
 
