16f7b969deb739f5568b0e9eea9f5416a413d327
Prince Roshan <princeroshan7@outlook.com>
2021-03-23 12:43:20 +0530

    Sealed Class Example

---
+++ SealedClass/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SealedClass
+{
+    sealed class Program
+    {
+        public int Add(int a, int b)
+        {
+            int res = a + b;
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            
+            Console.WriteLine("Addition"+ obj.Add(10,20));
+        }
+    }
+}
