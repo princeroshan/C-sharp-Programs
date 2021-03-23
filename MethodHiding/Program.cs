96f2f0aed8a1f95fc5d74091dd4e11c848dd3079
Prince Roshan <princeroshan7@outlook.com>
2021-03-23 14:12:41 +0530

    Method Hiding Example

---
+++ MethodHiding/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MethodHiding
+{
+    class A
+    {
+        public void Show()
+        {
+            Console.WriteLine("Base Class Method");
+        }
+    }
+    class B:A
+    {
+        public new void Show()
+        {
+            base.Show(); // call hidden base class method
+            Console.WriteLine("Derived Class Method");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A obj = new A(); //base class
+            //A obj = new B(); //base class
+            B obj = new B();
+            obj.Show(); //both base and derived method
+        }
+    }
+}
---
+++ MethodOverridngKeywords/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MethodOverridngKeywords
+{
+    class A
+    {
+        virtual public void Show()
+        {
+            Console.WriteLine("Base Class Method");
+        }
+    }
+    class B:A
+    {
+        override public void Show()
+        {
+            Console.WriteLine("Derived Class Method");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A obj = new A();
+            obj.Show();
+            A obj1 = new B();
+            obj1.Show();
+            B obj2 = new B();
+            obj2.Show();
+        }
+    }
+}
