73ae038caff4b1fdb79504d583062f8935d5c677
Prince Roshan <princeroshan7@outlook.com>
2021-03-23 13:41:33 +0530

    Method returning object type

---
+++ ObjectReturnMethod/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ObjectReturnMethod
+{
+    class Example
+    {
+        private string str;
+        public void setData(string st)
+        {
+            str = st;
+        }
+        public void Display()
+        {
+            Console.WriteLine("String is :" + str);
+        }
+        public Example ObjectMethodEx(Example obj)
+        {
+            Example ex = new Example();
+            ex.str = str+" " + obj.str; // to access str we have to create instance of class
+            return ex; //returns object
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Example ob1 = new Example();
+            ob1.setData("Geeks");
+            Example ob2 = new Example();
+            ob2.setData("For Geeks");
+            Example ob3 = ob1.ObjectMethodEx(ob2);
+            ob1.Display();
+            ob2.Display();
+            ob3.Display();
+        }
+    }
+}
