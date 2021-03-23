ce3a3e32064ec40a9fd6f56c585b521026a1014b
Prince Roshan <princeroshan7@outlook.com>
2021-03-23 12:32:50 +0530

    Object Class Program

---
+++ ObjectClass/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ObjectClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Object obj = new Object();
+            Object obj1 = new Object();
+            Object obj2 = new Object();
+            obj = "Manoj";
+            obj1 = "Suresh";
+            obj2="Manoj";
+            Console.WriteLine(obj.Equals(obj2)); //true
+            Console.WriteLine(obj.Equals(obj1)); //false
+            Console.WriteLine(Equals(obj,obj1)); //false
+            int i = 10;
+            Type t1 = obj.GetType();
+            Type t2 = obj.GetType();
+            Console.WriteLine("Object Type");
+            Console.WriteLine("Base Type"+t1.BaseType);
+            Console.WriteLine("Name"+t1.Name);
+            Console.WriteLine("FullName"+t1.FullName);
+            Console.WriteLine("Namespace"+t1.Namespace);
+            Console.WriteLine("Integer Type");
+            Console.WriteLine("Base Type" + t2.BaseType);
+            Console.WriteLine("Name" + t2.Name);
+            Console.WriteLine("FullName" + t2.FullName);
+            Console.WriteLine("Namespace" + t2.Namespace);
+            Console.ReadKey();
+        }
+    }
+}
